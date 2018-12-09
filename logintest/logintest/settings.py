477014e062e0a11d552139f4316210b3ac9108a9
Rupesh4S <44293264+Rupesh4S@users.noreply.github.com>
2018-12-09 17:56:49 +0530

    Update settings.py

--- logintest/logintest/settings.py
+++ logintest/logintest/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = []
 #Email verification
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.googlemail.com'
-EMAIL_HOST_USER = 'somanarupesh32@gmail.com'
-EMAIL_HOST_PASSWORD = 'rupesh0422'
+EMAIL_HOST_USER = 'EMAIL'
+EMAIL_HOST_PASSWORD = 'PASSWORD'
 EMAIL_PORT = 587
 
 
